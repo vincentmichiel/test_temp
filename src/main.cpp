568c1077e12f66c46775bfada78783da60433f46
pier woudstra <pier.johannes@gmail.com>
2024-03-12 16:15:26 +0100

    initial commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,15 @@
+#include <iostream>
+
 int main (){
+    std::cout << "blabla" << std::endl;
+
     return 0;
 }
 
 
 class Second {
+
+    std::cout << "second class" << std::endl;
     // second class
 }
 class fooClass {
